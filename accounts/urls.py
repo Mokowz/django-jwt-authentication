c2483872aaa053b2bf8023e74c2fbd65b64b058c
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-05-31 14:25:04 +0300

    Add login view, working on the register view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import Home, MyTokenObtainPairView
+from .views import (
+  # LoginView, 
+  MyTokenObtainPairView
+)
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
@@ -7,7 +10,7 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-  path("", Home.as_view()),
+  # path("login/", LoginView.as_view()),
   path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
   path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
--- accounts/views.py
+++ accounts/views.py
@@ -2,20 +2,27 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .serializers import MyTokenObtainPairSerializer
 
 class MyTokenObtainPairView(TokenObtainPairView):
   serializer_class = MyTokenObtainPairSerializer
 
-# Create your views here.
-class Home(APIView):
 
-  def get(self, request):
-    users = [
-      "Ronny",
-      "Lala",
-      "Mike"
-    ]
+# class LoginView(APIView):
 
-    return Response(users)
+#   def post(self, request):
+#     email = request.data.get('email')
+#     password = request.data.get('password')
+#     user = authenticate(email=email, password=password)
+#     refresh = RefreshToken.for_user(user)
+
+#     tokens = {
+#       "access": str(refresh),
+#       "refresh": str(refresh.access_token),
+#     }
+
+#     return Response(tokens)
+
