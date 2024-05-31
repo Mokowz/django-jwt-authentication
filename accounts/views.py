6117de569817cb9ecc35d9f7e678d005926b4fe4
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-05-31 14:04:20 +0300

    Customize the token claim to include the email

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import MyTokenObtainPairSerializer
+
+class MyTokenObtainPairView(TokenObtainPairView):
+  serializer_class = MyTokenObtainPairSerializer
 
 # Create your views here.
 class Home(APIView):
