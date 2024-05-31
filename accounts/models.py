f4039e830c13ae4e2353ba48ed6bde13a3b1c286
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-05-31 13:06:52 +0300

    Configure the base manager to use

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class CustomUser(AbstractBaseUser):
+  email = models.EmailField(unique=True)
+  first_name = models.CharField(max_length=255, blank=True, null=True)
+  last_name = models.CharField(max_length=255, blank=True, null=True)
+  date_joined = models.DateTimeField(auto_now_add=True)
+
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = []
