921753407562bfa493511b2ff383d89f14da8231
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-05-31 13:56:00 +0300

    Set the jwt

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 from .managers import CustomUserManager
 
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
   email = models.EmailField(unique=True)
   first_name = models.CharField(max_length=255, blank=True, null=True)
   last_name = models.CharField(max_length=255, blank=True, null=True)
@@ -23,4 +23,4 @@ class CustomUser(AbstractBaseUser):
     return f"{self.first_name} {self.last_name}"
 
   def __str__(self):
-    return self.full_name()
+    return self.email
